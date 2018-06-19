06f76798b77aabd3226c923aa50a26994a07c2b3
bjeong16 <bjeong16@utexas.edu>
2018-06-19 04:18:30 -0500

    Add files via upload
    Complete HangManGame

--- ExamplePython/HangManGame.py
+++ ExamplePython/HangManGame.py
@@ -4,25 +4,37 @@ import random
 
 def startGame(question):
     tries = len(question) * 3
-    currentState = "_"*len(question)
+    currentstate = list()
+    for x in question:
+        currentstate.append("_")
     while tries != 0:
-        print(currentState)
-        option = raw_input("To guess the entire word, press 1 \n To guess a letter in the word press 2 \n To give up and see the word press 3 \n")
+        print("You now have " + str(tries) + " tries left")
+        print(currentstate)
+        option = input("To guess the entire word, press 1 \nTo guess a letter in the word press 2 \nTo give up and see the word press 3 \n")
         if option == 1:
             guess = raw_input("Please enter your guess: \n")
             if guess == question:
                 print("Congratulations, you have saved Hangman")
+                return
             else:
-                print("That is not the write word. You now have " + str(tries - 1) + "chances left \n")
-                tries -= tries
+                print("That is not the right word. You now have " + str(tries - 1) + "chances left \n")
+                tries -= 1
         if option == 2:
             guess = raw_input("Please enter a character: \n")
+            counter = 0
             for letter in question:
-                if guess == option:
-
+                if guess == letter:
+                    currentstate[counter] = letter
+                counter += 1
+            tries -=1
         if option == 3:
             print(question)
+            print("Hangman Died")
+            return
 
+    print("Hangman Died")
+    print("The correct answer was: " + question)
+    return
 
 
 
@@ -32,12 +44,10 @@ if __name__ == '__main__':
     text_file = open("Dict.txt", 'r')
     mysteryWord = text_file.read()
     mysteryWord = mysteryWord.split()
-    mysteryWord = mysteryWord.translate(None, "\n")
-
     for word in mysteryWord:
         length = len(word)
         if length < 5:
-            mysteryWord.translate(word, "")
+            mysteryWord.remove(word)
 
     count = len(mysteryWord)
     index = random.randint(0, count)
@@ -47,8 +57,8 @@ if __name__ == '__main__':
     for x in range(count):
         print("_ ")
 
-    print("There are " + str(count) + "letters in the word")
-    print("You get" + str(count*3) + "tries to guess the word")
+    print("There are " + str(count) + " letters in the word")
+    print("You get " + str(count*3) + " tries to guess the word")
 
     startGame(question)
 
